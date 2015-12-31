f44a08e05b231ea8e55b7c80099ab70d12a65ce5
Me <hpatel90@illinois.edu>
2015-12-31 15:26:14 -0600

    Upload images to s3

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="content-background" style="padding-bottom: 60px;">
     <div class="row">
         <hr>
-        <div class="col-md-6 small"><a href="/privacy-policy">Privacy Policy</a> · <a href="/terms-conditions">Terms and Conditions</a> · <a href="/contact-us">Contact</a> </div>
-        <div class="col-md-4 col-md-offset-2 small"><div class="pull-right">2016 · {{Config::get('app.name')}}</div></div>
+        <div class="col-sm-6 small"><a href="/privacy-policy">Privacy Policy</a> · <a href="/terms-conditions">Terms and Conditions</a> · <a href="/contact-us">Contact</a> </div>
+        <div class="col-sm-4 col-sm-offset-2 small"><div class="pull-right">2016 · {{Config::get('app.name')}}</div></div>
     </div>
 </div>
