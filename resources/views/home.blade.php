a1a36bcdb042608d137b33bdaff433a653cc09af
Me <hpatel90@illinois.edu>
2016-01-02 16:23:45 -0600

    Added about page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,11 +62,9 @@
         </div>
 
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="row" style="padding-bottom: 30px; padding-top: 30px;">
-                    <div class="col-md-6 small"><a href="/privacy-policy">Privacy Policy</a> · <a href="/terms-conditions">Terms and Conditions</a> · <a href="/contact-us">Contact</a> </div>
-                    <div class="col-md-4 col-md-offset-2 small"><div class="pull-right">2016 · {{Config::get('app.name')}}</div></div>
-                </div>
+            <div class="col-md-10 col-md-offset-1 small" style="margin-top: 30px;"><a href="/privacy-policy">
+                    Privacy Policy</a> · <a href="/terms-conditions">Terms and Conditions</a>
+                <span class="pull-right">2016 · {{Config::get('app.name')}} · <a href="/about">About</a> · <a href="/contact-us">Contact</a></span>
             </div>
         </div>
 
