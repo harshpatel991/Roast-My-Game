b769d809ee1422c7306b5b5ed54d41b8f8c6c279
Me <hpatel90@illinois.edu>
2016-01-02 21:37:43 -0600

    Update home to show most updated games, remove justification from text boxes, remove <p> from text boxes

--- rmg.sh
+++ rmg.sh
@@ -35,11 +35,12 @@ localdev() {
 
 backupremotedb() {
   printf "${RED}------------Backup Remote DB------------${NC}\n"
-  #backup remote db
+  backupremotedb
   echo "User: ***REMOVED***. Enter DB password..."
   read dbpassword
   dt_now=$(date '+%d_%m_%Y__%H_%M_%S');
-  mysqldump --user=***REMOVED*** -p$dbpassword --host=rmg2.cwqtmomh10su.us-west-2.rds.amazonaws.com --protocol=tcp --port=3306 --default-character-set=utf8 "rmg" -r "./prod-db-backups/backup$dt_now.sql"
+  mysqldump --single-transaction --user=***REMOVED*** -p$dbpassword --host=rmg2.cwqtmomh10su.us-west-2.rds.amazonaws.com --protocol=tcp --port=3306 --default-character-set=utf8 "rmg" -r "./prod-db-backups/backup$dt_now.sql"
+  echo "Completed"
   echo "Copy remote dump to local test folder? [y/n]"
   read continue
 
