2727f1af0b56eafc953f3454ed28a6d1eb15e26d
Me <hpatel90@illinois.edu>
2016-01-13 18:58:37 -0600

    Add dev blog, twitter link to footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,7 +83,14 @@
             <hr>
             <div class="col-md-10 col-md-offset-1 small">
                 <div class="pull-right">
-                    2016 · {{Config::get('app.name')}} · <a href="/about">About</a> · <a href="/contact-us">Contact</a>
+                    <a href="/about">About</a> · <a href="/contact-us">Contact</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1 small">
+                <div class="pull-right">
+                    <a href="http://roastmygame.blogspot.com">Dev Blog</a> · <a href="https://twitter.com/RoastMyGame">Twitter</a>
                 </div>
             </div>
         </div>
