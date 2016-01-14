7a3063e7d53d3251251c00dfaadc8d1702ff9321
Me <hpatel90@illinois.edu>
2016-01-13 23:05:22 -0600

    Roasted email campaign tracking

--- resources/views/emails/comment-added-plain-text.blade.php
+++ resources/views/emails/comment-added-plain-text.blade.php
@@ -3,7 +3,7 @@ Hey there!
 Someone has roasted {{$game->title}}.
 <br><br>
 View it by clicking the link below. Be sure to pay it forward by roasting another game.
-{{ URL::to('game/' . $game->slug) }}
+{{ URL::to('game/' . $game->slug . '?utm_source=comment&utm_medium=email&utm_campaign=game_roasted') }}
 
 
 <br><br>
