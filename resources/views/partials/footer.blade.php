405c7d9034e652cb5ac9f403db740ba5fef0c8c7
Me <hpatel90@illinois.edu>
2016-01-13 18:58:37 -0600

    Add dev blog, twitter link to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,15 +1,20 @@
-<div class="content-background" style="padding-bottom: 60px;">
+<div class="content-background" style="padding-bottom: 30px;">
     <div class="row">
         <hr>
         <div class="col-xs-12 small">
             <div class="pull-right">
-                2016 · {{Config::get('app.name')}} · <a href="/about">About</a> · <a href="/contact-us">Contact</a>
+                 <a href="/about">About</a> · <a href="/contact-us">Contact</a>
             </div>
         </div>
-
     </div>
     <div class="row">
-
+        <div class="col-xs-12 small">
+            <div class="pull-right">
+                <a href="http://roastmygame.blogspot.com">Dev Blog</a> · <a href="https://twitter.com/RoastMyGame">Twitter</a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
         <div class="col-xs-12 small">
             <div class="pull-right">
                 <a href="/privacy-policy">Privacy Policy</a> · <a href="/terms-conditions">Terms and Conditions</a>
