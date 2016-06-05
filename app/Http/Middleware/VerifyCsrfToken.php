bbb1025ec50799fd76fe05530caf7e2c6ac32b03
Me <hpatel90@illinois.edu>
2016-06-05 14:11:49 -0500

    Add path logging for Verify CSRF Token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Log;
+use Illuminate\Support\Str;
 
 class VerifyCsrfToken extends BaseVerifier
 {
@@ -12,4 +14,33 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = ['/add-game'];
+
+    /**
+     * Determine if the session and input CSRF tokens match.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function tokensMatch($request)
+    {
+        $sessionToken = $request->session()->token();
+
+        $token = $request->input('_token') ?: $request->header('X-CSRF-TOKEN');
+
+        if (! $token && $header = $request->header('X-XSRF-TOKEN')) {
+            $token = $this->encrypter->decrypt($header);
+        }
+
+        if (! is_string($sessionToken) || ! is_string($token)) {
+            Log::warning('Token missing at path: '. $request->path());
+            return false;
+        }
+
+        $tokenMatches = Str::equals($sessionToken, $token);
+        if(!$tokenMatches) {
+            Log::warning('Token mismatch at path: '. $request->path());
+            return false;
+        }
+        return true;
+    }
 }
