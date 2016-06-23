e730a5717c1c17225790c6a2a7c2eb0455b12038
Me <hpatel90@illinois.edu>
2016-06-22 22:50:43 -0500

    Larger promote buttons

--- resources/views/promote.blade.php
+++ resources/views/promote.blade.php
@@ -32,10 +32,11 @@
                     <h6 class="subheading subheading-color">Website Embed</h6>
 
                     <p class="small">Copy this code into your websites HTML page to direct your users to roast your game.</p>
-                    <a href="{{ secure_url('/game/'.$game->slug) }}"><img src="https://s3-us-west-2.amazonaws.com/rmg-upload/public/share_button.jpg" class="center-block play-button-text-content"></a>
+                    <a href="{{ secure_url('/game/'.$game->slug) }}"><img width="150px" height="45px" src="https://s3-us-west-2.amazonaws.com/rmg-upload/public/share_button.png" class="center-block"></a>
+                    <br>
                     <div class="row">
                         <div class="col-sm-10">
-                            <input id="website-contents" class="form-control placeholder-dark" type="text" value="{{'<a href="'. secure_url('/game/'.$game->slug) .'"><img src="https://s3-us-west-2.amazonaws.com/rmg-upload/public/share_button.jpg"></a>'}}" readonly="readonly">
+                            <input id="website-contents" class="form-control placeholder-dark" type="text" value="{{'<a href="'. secure_url('/game/'.$game->slug) .'"><img width="150px" height="45px" src="https://s3-us-west-2.amazonaws.com/rmg-upload/public/share_button.png"></a>'}}" readonly="readonly">
                         </div>
                         <div class="col-sm-2">
                             <button id="btn-copy-website" class="btn btn-success-outline btn-block" data-clipboard-target="#website-contents"><span class="icon-clipboard"></span> Copy</button>
@@ -45,11 +46,11 @@
                     <h6 class="subheading subheading-color">Forum Embed</h6>
 
                     <p class="small">Copy this code into your forum submission to direct your readers to roast your game.</p>
-                    <a href="{{ secure_url('/game/'.$game->slug) }}"><img src="https://s3-us-west-2.amazonaws.com/rmg-upload/public/share_button.jpg" class="center-block play-button-text-content"></a>
-
+                    <a href="{{ secure_url('/game/'.$game->slug) }}"><img width="150px" height="45px" src="https://s3-us-west-2.amazonaws.com/rmg-upload/public/share_button.png" class="center-block"></a>
+                    <br>
                     <div class="row">
                         <div class="col-sm-10">
-                            <input id="forum-contents" class="form-control placeholder-dark" type="text" value="{{"[URL='". secure_url('/game/'.$game->slug) ."'][IMG]https://s3-us-west-2.amazonaws.com/rmg-upload/public/share_button.jpg[/IMG] [/URL]"}}" readonly="readonly">
+                            <input id="forum-contents" class="form-control placeholder-dark" type="text" value="{{"[URL='". secure_url('/game/'.$game->slug) ."'][IMG]https://s3-us-west-2.amazonaws.com/rmg-upload/public/share_button.png[/IMG] [/URL]"}}" readonly="readonly">
                         </div>
                         <div class="col-sm-2">
                             <button id="btn-copy-forum" class="btn btn-success-outline btn-block" data-clipboard-target="#forum-contents"><span class="icon-clipboard"></span> Copy</button>
