0fc4f79212d2043de87cf44275799b066cd62a92
Me <hpatel90@illinois.edu>
2017-04-30 01:08:22 -0500

    Change copy for adblocked users

--- resources/views/partials/ads.blade.php
+++ resources/views/partials/ads.blade.php
@@ -6,12 +6,10 @@
 
 <div class="small-red-box" style="display:none" id="ad-block-message">
     <div class="text-center" style="color: #fff; font-weight: bold;">
-        Hi there, it looks like you're using an adblocker.
+        View ad. Be upset. Keep us running.
     </div>
     <div class="text-center small" style="color: #fff;">
-        Roast My Game costs $40 a month to operate. Since most users use an adblocker,
-        I have to pay this out of pocket.
-        I would really appreciate it if you could disable your adblocker for this site.
+        You may not like this ad, but it supports the developer and keeps this site free.
     </div>
 
     <button type="button" class="btn btn-default center-block" data-toggle="modal" data-target="#exampleModal">Okay, I turned it off</button>
