a56f80cd1351f3b1d8d3cc210f7f873ed39797d7
harshpatel991 <hpatel90@illinois.edu>
2018-01-12 22:32:07 -0600

    Add Tweet Pup skyscraper and square ads

--- resources/views/partials/ads.blade.php
+++ resources/views/partials/ads.blade.php
@@ -1,16 +1,9 @@
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-6621729644063575"
-     data-ad-slot="2823513599"
-     data-ad-format="auto"></ins>
-
-<div class="small-red-box" style="display:none" id="ad-block-message">
-    <div class="text-center" style="color: #fff; font-weight: bold;">
-        View ad. Be upset. Keep us running.
-    </div>
-    <div class="text-center small" style="color: #fff;">
-        You may not like this ad, but it supports the developer and keeps this site free.
-    </div>
-
-    <button type="button" class="btn btn-default center-block" data-toggle="modal" data-target="#exampleModal">Okay, I turned it off</button>
-</div>
+@if(rand(0, 1) === 0)
+    <a href="https://tweetpup.com/?utm_source=rmg&utm_medium=square">
+        <img src="/images/square.jpg" style="max-width: 100%; padding-top: 20px;" class="center-block"/>
+    </a>
+@else
+    <a href="https://tweetpup.com/?utm_source=rmg&utm_medium=skyscraper">
+        <img src="/images/skyscraper.jpg" style="max-width: 70%; padding-top: 20px;" class="center-block"/>
+    </a>
+@endif
