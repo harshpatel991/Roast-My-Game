c4e497706e617cd246156ac537d9d5c51ae0b5a0
harshpatel991 <hpatel90@illinois.edu>
2018-04-07 18:40:46 -0500

    Exclude Tweet Pup 50% of the time

--- resources/views/partials/ads.blade.php
+++ resources/views/partials/ads.blade.php
@@ -1,9 +1,5 @@
-@if(rand(0, 1) === 0)
+@if(rand(0, 1) === 0) {{-- 50% chance of not displaying --}}
     <a href="https://tweetpup.com/?utm_source=rmg&utm_medium=square">
         <img src="/images/square.jpg" style="max-width: 100%; padding-top: 20px;" class="center-block"/>
-    </a>
-@else
-    <a href="https://tweetpup.com/?utm_source=rmg&utm_medium=skyscraper">
-        <img src="/images/skyscraper.jpg" style="max-width: 70%; padding-top: 20px;" class="center-block"/>
     </a>
 @endif
